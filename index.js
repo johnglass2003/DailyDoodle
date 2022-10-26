b3fcb641324b13cdee6cb50d428a73db0f38b928
johnglass2003 <50562358+johnglass2003@users.noreply.github.com>
2022-10-26 17:28:55 -0400

    Fix react

--- index.js
+++ index.js
@@ -29,34 +29,6 @@ submitButton.addEventListener('click', function (){
   console.log(dataURI);
 
   
-/*
-    let formData = new FormData();           
-    formData.append("file", fileupload.files[0]);
-    await fetch('/upload.php', 
-    {
-      method: "POST", 
-      body: formData
-    });
-
-  location.href = "doodles.html";
-
-  
-
-  var blob = dataURItoBlob(dataURI);
-  var fd = new FormData(document.forms[0]);
-  var xhr = new XMLHttpRequest();
-
-  var today = new Date();
-  var date = today.getFullYear()+(today.getMonth()+1)+today.getDate();
-  var time = today.getHours()+today.getMinutes()+today.getSeconds();
-  var dateTime = date+time;
-
-
-  fd.append(dateTime, blob);
-  xhr.open('POST', '/', true);
-  xhr.send(fd);
-  */
-  
 });
 
 function downloadImage(url) {
@@ -137,26 +109,3 @@ canvas.addEventListener('mouseleave', (e) => {
   });
 
 canvas.addEventListener('mousemove', draw);
-/*
-$('.page').each(function(i,e){
-    $(this).click(function(event){
-      var x = event.pageX;
-      var y = event.pageY;
-      
-      var nextItem = i + 1;
-      if (nextItem >= $('.page').length){
-        nextItem = 0;
-      }
-      
-      $('.page:eq('+ nextItem +')').css('z-index', parseInt($(this).css('z-index')) + 1);
-      $('.page:eq('+ nextItem +')').css('clip-path', 'circle(0% at '+ x +'px '+ y +'px)');
-      
-      anime({
-        targets: $('.page')[nextItem],
-        update: function(anim) {
-          $('.page:eq('+ nextItem +')').css('clip-path', 'circle('+ (anim.progress*2) +'% at '+ x +'px '+ y +'px)');
-        }
-      });
-    });
-  });
-  */
